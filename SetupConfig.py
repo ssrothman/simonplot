8dd39d3b0bfac87eed7e4cf9fecacf7c2534d21b
Simon Rothman <srothman@mit.edu>
2025-12-10 13:47:37 -0500

    Also escape parenthesis in regex matching

--- SetupConfig.py
+++ SetupConfig.py
@@ -7,10 +7,18 @@ with open("simon_mpl_config.json", "r") as f:
 def lookup_axis_label(axiskey: str) -> str:
     #use regex to match axiskey with config keys:
     for key_pattern, label in config['axis_labels'].items():
+        
         key_pattern = key_pattern.replace('.', r'\.')  #escape dots
+        #escape parentheses
+        key_pattern = key_pattern.replace('(', r'\(')
+        key_pattern = key_pattern.replace(')', r'\)')
+
         key_pattern = key_pattern.replace("*", "[a-zA-Z0-9]*")  #convert wildcard * to regex for any alphanumeric characters [but NOT underscores]
-        #need to match with no trailing characters:
+        
+        #need to match with no leading or trailing characters:
         key_pattern = "^" + key_pattern + "$"
+        
         if re.fullmatch(key_pattern, axiskey):
             return label
+        
     return axiskey  #default to axiskey if no match found
